67b36c7b7b46e1c9322981dfe49cdf354db29286
RomanV1 <gnida2404@yandex.ru>
2022-12-29 01:50:50 +0300

    added Postgres && deleted MySQL

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,14 +1,14 @@
-import { pool } from "./connect";
-import {IUser} from "../models/users.entity";
+import pool from "./dbConfig";
+import { IUser } from "../models/users.entity";
 
 export class UserService {
-    async getUsers(): Promise<IUser[] | undefined> {
-        const [users] = await pool.query<IUser[]>("SELECT id, login, email FROM users");
-        return users
+    async getUsers(): Promise<IUser[]> {
+        const { rows } = await pool.query<IUser[]>("SELECT id, login, email FROM users");
+        return rows[0]
     }
 
-    async getUserById(id: string): Promise<IUser[] | undefined> {
-        const [users] = await pool.query<IUser[]>(`SELECT id, login, email FROM users WHERE id = ${id}`);
-        return users
+    async getUserById(id: string): Promise<IUser[]> {
+        const { rows } = await pool.query<IUser[]>(`SELECT id, login, email FROM users WHERE id = ${id}`);
+        return rows[0]
     }
 }
