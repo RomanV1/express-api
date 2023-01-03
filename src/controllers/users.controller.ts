decfa8eae496cb54ff54ccb510b02b6da2bd4907
RomanV1 <gnida2404@yandex.ru>
2023-01-04 00:39:30 +0300

    added validate data

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,66 +1,76 @@
 import { Request, Response } from 'express'
-import { UserService } from '../services/users.service'
+import { UsersService } from '../services/users.service'
 import { IUser } from '../models/users.entity'
 import { RequestWithBody, RequestWithParams } from '../models/typedRequests'
 import { IUserBody } from '../models/users.interface'
 
 class UsersController {
+    // private usersService: UsersService
+    // constructor() {
+    //     this.usersService = new UsersService()
+    // }
     async getUsers(req: Request, res: Response) {
         try {
-            const users: IUser[] | undefined = await new UserService().getUsers()
+            const users: IUser[] | undefined = await new UsersService().getUsers()
             if (users && users.length === 0) {
                 res.status(404).json({ error: 'Users is not found' })
                 return
             }
+
             res.status(200).send(users)
         } catch (e) {
-            console.log(e)
             res.status(500).json({ error: "Server's error" })
+            console.log('Error! Method: getUsers \n' + e)
         }
     }
 
     async getUserById(req: RequestWithParams<{ id: string }>, res: Response) {
         try {
             const { id } = req.params
-            const user: IUser[] | undefined = await new UserService().getUserById(id)
+            if (!Number(id)) {
+                res.status(400).json({ error: 'Bad Request' })
+                return
+            }
+
+            const user: IUser[] | undefined = await new UsersService().getUserById(id)
             if (user && user.length === 0) {
                 res.status(404).json({ error: 'User is not found' })
                 return
             }
+
             res.status(200).send(user)
         } catch (e) {
-            console.log(e)
             res.status(500).json({ error: "Server's error" })
+            console.log('Error! Method: getUserById \n' + e)
         }
     }
 
     async createUser(req: RequestWithBody<IUserBody>, res: Response): Promise<void> {
         try {
             const { login, email, password } = req.body
-
-            const isUniqueData: boolean | undefined = await new UserService().isUniqueData(login, email)
-            console.log(isUniqueData)
-            if (!isUniqueData && isUniqueData !== undefined) {
-                res.status(300).json({ message: 'User is already exist. Change your login or email' })
-                return
-            }
-            if (isUniqueData === undefined) {
+            if (typeof login === 'undefined' || typeof email === 'undefined' || typeof password === 'undefined') {
                 res.status(400).json({ error: 'Bad Request' })
                 return
             }
 
-            await new UserService()
+            const isUniqueData: boolean | undefined = await new UsersService().isUniqueData(login, email)
+            if (!isUniqueData) {
+                res.status(400).json({ message: 'User is already exist. Change your login or email' })
+                return
+            }
+
+            await new UsersService()
                 .createUser(login, email, password)
                 .then(() => {
                     return res.status(201).json({ message: 'User has been created' })
                 })
                 .catch((e) => {
                     console.log(e)
-                    return res.status(400).json({ error: 'Bad Request' })
+                    return res.status(500).json({ error: "Server's error" })
                 })
         } catch (e) {
-            console.log(e)
             res.status(500).json({ error: "Server's error" })
+            console.log('Error! Method: createUser \n' + e)
         }
     }
 }
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,38 +1,48 @@
-import pool from './dbConfig'
 import { IUser } from '../models/users.entity'
+import { Pool } from 'pg'
+import dotenv from 'dotenv'
+dotenv.config()
 
-export class UserService {
+export class UsersService {
+    private pool: Pool
+    constructor() {
+        this.pool = new Pool({
+            host: process.env.PG_HOST,
+            user: process.env.PG_USER,
+            password: process.env.PG_PASSWORD,
+            database: process.env.PG_DATABASE,
+            idleTimeoutMillis: 3000
+        })
+    }
+
     async getUsers(): Promise<IUser[] | undefined> {
         try {
-            const { rows } = await pool.query<IUser>('SELECT id, login, email FROM users')
+            const { rows } = await this.pool.query<IUser>('SELECT id, login, email FROM users')
             return rows
         } catch (e) {
-            console.log(e)
+            console.log('Error! Method: getUsers \n' + e)
         }
     }
 
     async getUserById(id: string): Promise<IUser[] | undefined> {
         try {
-            const { rows } = await pool.query<IUser>(`SELECT id, login, email FROM users WHERE id = ${id}`)
+            const { rows } = await this.pool.query<IUser>(`SELECT id, login, email FROM users WHERE id = ${id}`)
             return rows
         } catch (e) {
-            console.log(e)
+            console.log('Error! Method: getUserById \n' + e)
         }
     }
 
     async createUser(login: string, email: string, hash: string): Promise<void> {
-        await pool.query<IUser[]>(`INSERT INTO users (login, email, hash) VALUES ($1, $2, $3)`, [login, email, hash])
+        await this.pool.query<IUser[]>(`INSERT INTO users (login, email, hash) VALUES ($1, $2, $3)`, [login, email, hash])
     }
 
     async isUniqueData(login: string, email: string): Promise<boolean | undefined> {
         try {
-            if (typeof login === 'undefined' || typeof email === 'undefined') {
-                return undefined
-            }
-            const { rows } = await pool.query(`SELECT login, email FROM users WHERE login = '${login}' OR email = '${email}'`)
+            const { rows } = await this.pool.query(`SELECT login, email FROM users WHERE login = '${login}' OR email = '${email}'`)
             return rows.length === 0
         } catch (e) {
-            console.log(e)
+            console.log('Error! Method: isUniqueData \n' + e)
         }
     }
 }
