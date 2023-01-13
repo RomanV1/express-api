434f12cf23c9d239f484d05a6a821490e3a4214a
RomanV1 <gnida2404@yandex.ru>
2023-01-13 12:34:23 +0300

    added type RequestWithParamsAndBody

--- src/models/typedRequests.ts
+++ src/models/typedRequests.ts
@@ -3,3 +3,5 @@ import { Request } from 'express'
 export type RequestWithParams<T> = Request<T>
 
 export type RequestWithBody<T> = Request<{}, {}, T>
+
+export type RequestWithParamsAndBody<T, K> = Request<T, {}, K>
