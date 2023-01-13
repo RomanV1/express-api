81d2fabc5faad4be1200b1b0dfdd7b604f89cbab
RomanV1 <gnida2404@yandex.ru>
2023-01-13 12:35:20 +0300

    added deleteUser && updateUser methods

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 import { UsersService } from '../services/users.service'
 import { IUser } from '../models/users.entity'
-import { RequestWithBody, RequestWithParams } from '../models/typedRequests'
+import { RequestWithBody, RequestWithParams, RequestWithParamsAndBody } from '../models/typedRequests'
 import { IUserBody } from '../models/users.interface'
 
 export class UsersController {
@@ -34,7 +34,7 @@ export class UsersController {
                 return
             }
 
-            const user: IUser[] | undefined = await new UsersService().getUserById(id)
+            const user: IUser[] | undefined = await this.usersService.getUserById(id)
             if (user && user.length === 0) {
                 res.status(404).json({ error: 'User is not found' })
                 return
@@ -55,24 +55,92 @@ export class UsersController {
                 return
             }
 
-            const isUniqueData: boolean | undefined = await new UsersService().isUniqueData(login, email)
-            if (!isUniqueData) {
+            const isUserExist: boolean | undefined = await this.usersService.isUserExist(login, email)
+            if (isUserExist && typeof isUserExist !== 'undefined') {
                 res.status(400).json({ message: 'User is already exist. Change your login or email' })
                 return
             }
+            if (typeof isUserExist === 'undefined') {
+                res.status(500).json({ error: "Server's error" })
+                return
+            }
 
-            await new UsersService()
+            await this.usersService
                 .createUser(login, email, password)
                 .then(() => {
-                    return res.status(201).json({ message: 'User has been created' })
+                    res.status(201).json({ message: 'User has been created' })
+                    return
                 })
                 .catch((e) => {
                     console.log(e)
-                    return res.status(500).json({ error: "Server's error" })
+                    res.status(500).json({ error: "Server's error" })
+                    return
                 })
         } catch (e) {
             res.status(500).json({ error: "Server's error" })
             console.log('Error! Method: createUser \n' + e)
         }
+    }
+
+    async deleteUser(req: RequestWithParams<{ id: string }>, res: Response) {
+        const { id } = req.params
+        if (!Number(id)) {
+            res.status(400).json({ error: 'Bad request' })
+            return
+        }
+
+        const user: IUser[] | undefined = await this.usersService.getUserById(id)
+        if (user?.length === 0) {
+            res.status(400).json({ error: 'User deleted or not found' })
+            return
+        }
+
+        await this.usersService
+            .deleteUser(id)
+            .then(() => {
+                res.status(201).json({ message: 'User has been deleted' })
+                return
+            })
+            .catch((e) => {
+                console.log(e)
+                res.status(500).json({ error: "Server's error" })
+                return
+            })
+    }
+
+    async updateUser(req: RequestWithParamsAndBody<{ id: string }, IUserBody>, res: Response) {
+        const { id } = req.params
+        if (!Number(id)) {
+            res.status(400).json({ error: 'Bad request' })
+            return
+        }
+
+        const { login, email } = req.body
+        if (typeof login === 'undefined' || typeof email === 'undefined') {
+            res.status(400).json({ error: 'Bad request' })
+            return
+        }
+
+        const user: IUser[] | undefined = await this.usersService.getUserById(id)
+        if (user?.length === 0) {
+            res.status(400).json({ message: "User doesn't exist" })
+            return
+        }
+        if (typeof user === 'undefined') {
+            res.status(500).json({ error: "Server's error" })
+            return
+        }
+
+        await this.usersService
+            .updateUser(id, login, email)
+            .then(() => {
+                res.status(201).json({ message: 'User has been updated' })
+                return
+            })
+            .catch((e) => {
+                console.log(e)
+                res.status(500).json({ error: "Server's error" })
+                return
+            })
     }
 }
