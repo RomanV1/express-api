f9891b7f810768e709bca06fd2f83754691cee10
RomanV1 <gnida2404@yandex.ru>
2023-01-13 12:36:21 +0300

    added put && delete routers

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,13 @@
-import express from 'express'
+import express, { Request } from 'express'
 import { UsersController } from '../controllers/users.controller'
+import { RequestWithBody, RequestWithParams, RequestWithParamsAndBody } from '../models/typedRequests'
+import { IUserBody } from '../models/users.interface'
+
 export const router = express.Router()
 const usersController = new UsersController()
 
-router.get('/', (req, res) => usersController.getUsers(req, res))
-router.get('/:id', (req, res) => usersController.getUserById(req, res))
-router.post('/', (req, res) => usersController.createUser(req, res))
-// router.put('/', UsersController.createUser)
-// router.delete('/', UsersController.createUser)
+router.get('/', (req: Request, res) => usersController.getUsers(req, res))
+router.get('/:id', (req: RequestWithParams<{ id: string }>, res) => usersController.getUserById(req, res))
+router.post('/', (req: RequestWithBody<IUserBody>, res) => usersController.createUser(req, res))
+router.put('/:id', (req: RequestWithParamsAndBody<{ id: string }, IUserBody>, res) => usersController.updateUser(req, res))
+router.delete('/:id', (req: RequestWithParams<{ id: string }>, res) => usersController.deleteUser(req, res))
